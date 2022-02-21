3b4e0aa7774a480a880149ac20c1a12e81cb06f7
crazyjums <crazyjums@gmail.com>
2022-02-21 23:14:00 +0800

    update nginx and php-fpm

---
+++ nginx/html/php_nginx/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<h1>恭喜您，成功将nginx与php-fpm分成两个容器部署</h1>
+<?php
+echo "<h1>nginx连接php-fpm成功</h1>";
+phpinfo();
+?>
+</body>
+</html>
